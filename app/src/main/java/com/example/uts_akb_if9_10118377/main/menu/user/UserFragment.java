dece38df9a07478f16e45977d9ee51efeba527cf
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-06-03 17:10:43 +0700

    fixfixfix :)

--- app/src/main/java/com/example/uts_akb_if9_10118377/main/menu/user/UserFragment.java
+++ app/src/main/java/com/example/uts_akb_if9_10118377/main/menu/user/UserFragment.java
@@ -3,6 +3,7 @@ package com.example.uts_akb_if9_10118377.main.menu.user;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
@@ -10,9 +11,11 @@ import android.view.LayoutInflater;
 import com.example.uts_akb_if9_10118377.R;
 
 public class UserFragment extends Fragment {
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container) {
-        View root = inflater.inflate(R.layout.fragment_task, container, false);
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_user, container, false);
 
         return root;
     }
+
 }
