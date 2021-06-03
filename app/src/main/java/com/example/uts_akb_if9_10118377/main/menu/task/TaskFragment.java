6dc869bf62d3baf80c3b016a9bfcb1bb66931fa6
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-06-03 14:19:35 +0700

    create user, task and info fragment

---
+++ app/src/main/java/com/example/uts_akb_if9_10118377/main/menu/task/TaskFragment.java
@@ -0,0 +1,4 @@
+package com.example.uts_akb_if9_10118377.main.menu.task;
+
+public class TaskFragment {
+}
---
+++ app/src/main/java/com/example/uts_akb_if9_10118377/main/menu/user/UserFragment.java
@@ -0,0 +1,4 @@
+package com.example.uts_akb_if9_10118377.main.menu.user;
+
+public class UserFragment {
+}
