8d2b22a528500fe896cde9d380f2ee4cc14c62f6
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-06-03 13:32:14 +0700

    adding delay timeout on splash act

--- app/src/main/java/com/example/uts_akb_if9_10118377/splash/SplashActivity.java
+++ app/src/main/java/com/example/uts_akb_if9_10118377/splash/SplashActivity.java
@@ -2,9 +2,12 @@ package com.example.uts_akb_if9_10118377.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.uts_akb_if9_10118377.R;
+import com.example.uts_akb_if9_10118377.main.MainActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -14,5 +17,15 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
