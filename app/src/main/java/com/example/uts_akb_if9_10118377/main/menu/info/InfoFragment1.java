9a69e0ad5d6982cbeca141b0249e1a9489982f17
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-06-04 14:35:22 +0700

    create info fragment 1 2 3

---
+++ app/src/main/java/com/example/uts_akb_if9_10118377/main/menu/info/InfoFragment1.java
@@ -0,0 +1,22 @@
+package com.example.uts_akb_if9_10118377.main.menu.info;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.uts_akb_if9_10118377.R;
+
+public class InfoFragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_info_pages_1, container, false);
+
+        return root;
+    }
+}
