ed7b3fa77046fe0b175eb08370a409c0bb757cc3
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-06-04 16:10:06 +0700

    sudah bisa implement view pager

--- app/src/main/java/com/example/uts_akb_if9_10118377/main/MainActivity.java
+++ app/src/main/java/com/example/uts_akb_if9_10118377/main/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.uts_akb_if9_10118377.main;
 import android.os.Bundle;
 
 import com.example.uts_akb_if9_10118377.R;
+import com.example.uts_akb_if9_10118377.main.menu.info.InfoFragment1;
+import com.example.uts_akb_if9_10118377.main.menu.info.InfoFragment2;
+import com.example.uts_akb_if9_10118377.main.menu.info.InfoFragment3;
+import com.example.uts_akb_if9_10118377.main.menu.info.SlidePagerAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navView, navController);
+
     }
 
 }
--- app/src/main/java/com/example/uts_akb_if9_10118377/main/menu/info/InfoFragment.java
+++ app/src/main/java/com/example/uts_akb_if9_10118377/main/menu/info/InfoFragment.java
@@ -1,18 +1,45 @@
 package com.example.uts_akb_if9_10118377.main.menu.info;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
 
 import com.example.uts_akb_if9_10118377.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InfoFragment extends Fragment {
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container) {
-        View root = inflater.inflate(R.layout.fragment_task, container, false);
+
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_info, container, false);
+
+        viewPager = root.findViewById(R.id.pager);
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        List<Fragment> list = new ArrayList<>();
+        list.add(new InfoFragment1());
+        list.add(new InfoFragment2());
+        list.add(new InfoFragment3());
+
+        pagerAdapter = new SlidePagerAdapter(requireActivity().getSupportFragmentManager(), list);
+
+        viewPager.setAdapter(pagerAdapter);
     }
 }
