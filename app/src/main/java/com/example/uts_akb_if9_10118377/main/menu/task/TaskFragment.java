2356f35def0cbb53d40a51ad641d80f6659989ce
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-06-05 15:26:35 +0700

    adding event listener to add button

--- app/src/main/java/com/example/uts_akb_if9_10118377/main/menu/task/TaskFragment.java
+++ app/src/main/java/com/example/uts_akb_if9_10118377/main/menu/task/TaskFragment.java
@@ -4,18 +4,31 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
 
 import com.example.uts_akb_if9_10118377.R;
+import com.example.uts_akb_if9_10118377.createTask.CreateTaskActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TaskFragment extends Fragment {
+    private FloatingActionButton addButton;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_task, container, false);
+
+        addButton = root.findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), CreateTaskActivity.class));
+            }
+        });
 
         return root;
     }
