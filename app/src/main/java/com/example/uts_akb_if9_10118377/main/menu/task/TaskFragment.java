8d00aa6eccc8c7c69878b82c7314bde14039a156
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-06-05 12:16:53 +0700

    fix task fragment

--- app/src/main/java/com/example/uts_akb_if9_10118377/main/menu/task/TaskFragment.java
+++ app/src/main/java/com/example/uts_akb_if9_10118377/main/menu/task/TaskFragment.java
@@ -1,16 +1,20 @@
 package com.example.uts_akb_if9_10118377.main.menu.task;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
 
 import com.example.uts_akb_if9_10118377.R;
 
-public class TaskFragment extends Fragment{
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container) {
+public class TaskFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_task, container, false);
 
         return root;
