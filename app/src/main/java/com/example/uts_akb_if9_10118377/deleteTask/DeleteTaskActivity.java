034553bc223b7d7ffc2ca6f419b76c7a7b04a688
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-06-05 20:43:30 +0700

    create delete task act and xml

---
+++ app/src/main/java/com/example/uts_akb_if9_10118377/deleteTask/DeleteTaskActivity.java
@@ -0,0 +1,14 @@
+package com.example.uts_akb_if9_10118377.deleteTask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_task);
+    }
+}
