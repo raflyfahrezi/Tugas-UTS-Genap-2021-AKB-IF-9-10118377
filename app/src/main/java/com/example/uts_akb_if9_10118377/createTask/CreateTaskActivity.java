a5653e595900a93434c4860707e5b59dbb76a4ef
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-06-05 14:38:47 +0700

    create add task

---
+++ app/src/main/java/com/example/uts_akb_if9_10118377/createTask/CreateTaskActivity.java
@@ -0,0 +1,16 @@
+package com.example.uts_akb_if9_10118377.createTask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.uts_akb_if9_10118377.R;
+
+public class CreateTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_task);
+    }
+}
